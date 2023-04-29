dfe3fbe6fcc4fc9284c9940f3d76cd17f37c64b3
Robin Wersich <robin.wersich@student.hpi.de>
2023-04-29 14:18:47 +0200

    Add Task composable

--- app/src/main/kotlin/de/robinwersich/todue/ui/home/HomeScreenViewModel.kt
+++ app/src/main/kotlin/de/robinwersich/todue/ui/home/HomeScreenViewModel.kt
@@ -4,19 +4,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import de.robinwersich.todue.data.entities.Task
-import de.robinwersich.todue.data.repositories.TaskRepository
+import de.robinwersich.todue.data.repositories.DatabaseTaskRepository
 import de.robinwersich.todue.toDueApplication
+import de.robinwersich.todue.ui.TaskUiData
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
 class HomeScreenViewModel(
-  private val taskRepository: TaskRepository,
+  taskRepository: DatabaseTaskRepository,
 ) : ViewModel() {
-  val taskList: StateFlow<List<Task>> =
+  val taskList: StateFlow<List<TaskUiData>> =
     taskRepository
       .getAllTasks()
+      .map { tasks -> tasks.map { TaskUiData(it.id, it.text, it.dueDate, it.doneDate != null) } }
       .stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5_000),
