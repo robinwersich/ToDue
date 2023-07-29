fa5af605153e7ad35dbaf69a57ca0725b8c99b19
Robin Wersich <robin.wersich@student.hpi.de>
2023-07-29 12:24:47 +0200

    Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,21 +38,21 @@ android {
 }
 
 dependencies {
-  implementation("androidx.core:core-ktx:1.10.0")
-  implementation("androidx.activity:activity-compose:1.7.0")
+  implementation("androidx.core:core-ktx:1.10.1")
+  implementation("androidx.activity:activity-compose:1.7.2")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
   implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
 
   // Compose
-  val composeUiVersion = "1.4.1"
+  val composeUiVersion = "1.4.3"
   implementation("androidx.compose.ui:ui:$composeUiVersion")
   implementation("androidx.compose.ui:ui-tooling-preview:$composeUiVersion")
   debugImplementation("androidx.compose.ui:ui-tooling:$composeUiVersion")
   debugImplementation("androidx.compose.ui:ui-test-manifest:$composeUiVersion")
-  implementation("androidx.compose.material3:material3:1.0.1")
+  implementation("androidx.compose.material3:material3:1.1.1")
 
   // Room
-  val roomVersion = "2.5.1"
+  val roomVersion = "2.5.2"
   implementation("androidx.room:room-runtime:$roomVersion")
   ksp("androidx.room:room-compiler:$roomVersion")
   implementation("androidx.room:room-ktx:$roomVersion")
