5f7cad99d74f9412423a85f12edee5688e1836a8
Robin Wersich <robin.wersich@student.hpi.de>
2023-12-28 17:07:00 +0100

    Add initial timeline on DB creation

--- app/src/main/kotlin/com/robinwersich/todue/data/database/ToDueDatabase.kt
+++ app/src/main/kotlin/com/robinwersich/todue/data/database/ToDueDatabase.kt
@@ -1,10 +1,13 @@
 package com.robinwersich.todue.data.database
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.robinwersich.todue.data.entity.TaskEntity
 import com.robinwersich.todue.data.entity.TimelineEntity
 
@@ -19,10 +22,25 @@ abstract class ToDueDatabase : RoomDatabase() {
     fun getDatabase(context: Context): ToDueDatabase =
       Instance
         ?: synchronized(this) {
-          Room.databaseBuilder(context, ToDueDatabase::class.java, "todue_database")
+          Room.databaseBuilder(context, ToDueDatabase::class.java, "ToDue.db")
             .fallbackToDestructiveMigration()
+            .addCallback(CreateInitialTimelines)
             .build()
             .also { Instance = it }
         }
+  }
+}
+
+private object CreateInitialTimelines : RoomDatabase.Callback() {
+  override fun onCreate(db: SupportSQLiteDatabase) {
+    super.onCreate(db)
+    db.insert(
+      "timeline",
+      SQLiteDatabase.CONFLICT_ABORT,
+      ContentValues().apply {
+        put("time_block_unit", "DAY")
+        put("time_block_size", 1)
+      }
+    )
   }
 }
