6e2c36cd5ccae12d6b1f68753f09ea8880e906f5
Robin Wersich <robin.wersich@student.hpi.de>
2024-08-10 15:33:36 +0200

    Add narrow TimeBlock formatting

--- app/src/main/kotlin/com/robinwersich/todue/ui/utility/TimeBlockFormatter.kt
+++ app/src/main/kotlin/com/robinwersich/todue/ui/utility/TimeBlockFormatter.kt
@@ -34,7 +34,8 @@ fun rememberTimeBlockFormatter(): TimeBlockFormatter {
  * @param resources The resources of the current language to use for string formatting.
  */
 class TimeBlockFormatter(private val resources: Resources) {
-  private val formatters = Formatters.fromResources(resources)
+  private val wideFormatters = Formatters.fromResources(resources)
+  private val narrowFormatters = Formatters.fromResourcesNarrow(resources)
 
   /**
    * Formats a [TimeBlock] into a human-readable string.
@@ -43,47 +44,61 @@ class TimeBlockFormatter(private val resources: Resources) {
    * @param today The current date to use as a reference for today, tomorrow, this week, etc. and to
    *   determine if the year should be omitted. If null, no context is used and the year is always
    *   included.
+   * @param useNarrowFormatting Split the text over multiple lines to make it more narrow.
    */
-  fun format(timeBlock: TimeBlock, today: LocalDate? = LocalDate.now()): String =
+  fun format(
+    timeBlock: TimeBlock,
+    today: LocalDate? = LocalDate.now(),
+    useNarrowFormatting: Boolean = false,
+  ) = format(timeBlock, today, if (useNarrowFormatting) narrowFormatters else wideFormatters)
+
+  private fun format(
+    timeBlock: TimeBlock,
+    today: LocalDate? = LocalDate.now(),
+    formatters: Formatters,
+  ): String =
     when (timeBlock) {
-      is Day -> formatDay(timeBlock, today)
-      is Week -> formatWeek(timeBlock, today)
-      is Month -> formatMonth(timeBlock, today)
+      is Day -> formatDay(timeBlock, today, formatters)
+      is Week -> formatWeek(timeBlock, today, formatters)
+      is Month -> formatMonth(timeBlock, today, formatters)
       is TimeUnitInstanceSequence -> {
         when {
-          timeBlock.startBlock == timeBlock.endBlock -> format(timeBlock.startBlock, today)
+          timeBlock.startBlock == timeBlock.endBlock ->
+            format(timeBlock.startBlock, today, formatters)
           // if single weeks are represented as date range, we only need to format the outer range
-          timeBlock.unit == TimeUnit.WEEK -> formatDateRange(timeBlock, today)
-          else -> "${format(timeBlock.startBlock, today)} – ${format(timeBlock.endBlock, today)}"
+          timeBlock.unit == TimeUnit.WEEK -> formatDateRange(timeBlock, today, formatters)
+          else ->
+            format(timeBlock.startBlock, today, formatters) +
+              format(timeBlock.endBlock, today, formatters)
         }
       }
-      else -> formatDateRange(timeBlock, today)
+
+      else -> formatDateRange(timeBlock, today, formatters)
     }
 
-  private fun formatDay(day: Day, today: LocalDate?): String {
-    return when (day.date) {
-      today -> resources.getString(R.string.today)
-      today?.plusDays(1) -> resources.getString(R.string.tomorrow)
-      today?.minusDays(1) -> resources.getString(R.string.yesterday)
+  private fun formatDay(day: Day, today: LocalDate?, formatters: Formatters) =
+    when (day.date) {
+      today -> formatters.today
+      today?.plusDays(1) -> formatters.tomorrow
+      today?.minusDays(1) -> formatters.yesterday
       else -> {
         val isThisYear = day.date.year == today?.year
         val formatter = if (isThisYear) formatters.day else formatters.dayWithYear
         formatter.format(day.date)
       }
     }
-  }
 
-  private fun formatWeek(week: Week, today: LocalDate?): String {
+  private fun formatWeek(week: Week, today: LocalDate?, formatters: Formatters): String {
     val thisWeek = today?.let { YearWeek.from(it) }
     return when (week.yearWeek) {
-      thisWeek -> resources.getString(R.string.this_week)
-      thisWeek?.plusWeeks(1) -> resources.getString(R.string.next_week)
-      thisWeek?.minusWeeks(1) -> resources.getString(R.string.last_week)
-      else -> formatDateRange(week, today)
+      thisWeek -> formatters.thisWeek
+      thisWeek?.plusWeeks(1) -> formatters.nextWeek
+      thisWeek?.minusWeeks(1) -> formatters.lastWeek
+      else -> formatDateRange(week, today, formatters)
     }
   }
 
-  private fun formatDateRange(range: DateRange, today: LocalDate?): String {
+  private fun formatDateRange(range: DateRange, today: LocalDate?, formatters: Formatters): String {
     val isSameYear = range.start.year == range.endInclusive.year
     val isSameMonth = range.start.month == range.endInclusive.month && isSameYear
     val useStartYear = !isSameYear
@@ -103,10 +118,10 @@ class TimeBlockFormatter(private val resources: Resources) {
         else -> formatters.dateRangeEnd
       }
 
-    return "${startFormatter.format(range.start)} – ${endFormatter.format(range.endInclusive)}"
+    return startFormatter.format(range.start) + endFormatter.format(range.endInclusive)
   }
 
-  private fun formatMonth(month: Month, today: LocalDate?): String {
+  private fun formatMonth(month: Month, today: LocalDate?, formatters: Formatters): String {
     val useYear = month.yearMonth.year == today?.year
     val formatter = if (useYear) formatters.month else formatters.monthWithYear
     return formatter.format(month.yearMonth)
@@ -114,36 +129,73 @@ class TimeBlockFormatter(private val resources: Resources) {
 }
 
 private data class Formatters(
+  val yesterday: String,
+  val today: String,
+  val tomorrow: String,
+  val lastWeek: String,
+  val thisWeek: String,
+  val nextWeek: String,
   val day: DateTimeFormatter,
   val dayWithYear: DateTimeFormatter,
   val month: DateTimeFormatter,
   val monthWithYear: DateTimeFormatter,
   val year: DateTimeFormatter,
   val dateRangeStart: DateTimeFormatter,
+  val dateRangeStartSameMonth: DateTimeFormatter,
   val dateRangeStartWithYear: DateTimeFormatter,
   val dateRangeEnd: DateTimeFormatter,
-  val dateRangeEndWithYear: DateTimeFormatter,
-  val dateRangeStartSameMonth: DateTimeFormatter,
   val dateRangeEndSameMonth: DateTimeFormatter,
   val dateRangeEndSameMonthWithYear: DateTimeFormatter,
+  val dateRangeEndWithYear: DateTimeFormatter,
 ) {
   companion object {
     fun fromResources(resources: Resources) =
       with(resources) {
         Formatters(
+          yesterday = getString(R.string.yesterday),
+          today = getString(R.string.today),
+          tomorrow = getString(R.string.tomorrow),
+          lastWeek = getString(R.string.last_week),
+          thisWeek = getString(R.string.this_week),
+          nextWeek = getString(R.string.next_week),
           day = formatter(R.string.format_day),
           dayWithYear = formatter(R.string.format_day_with_year),
           month = formatter(R.string.format_month),
           monthWithYear = formatter(R.string.format_month_with_year),
           year = formatter(R.string.format_year),
           dateRangeStart = formatter(R.string.format_date_range_start),
+          dateRangeStartSameMonth = formatter(R.string.format_date_range_start_same_month),
           dateRangeStartWithYear = formatter(R.string.format_date_range_start_with_year),
           dateRangeEnd = formatter(R.string.format_date_range_end),
-          dateRangeEndWithYear = formatter(R.string.format_date_range_end_with_year),
-          dateRangeStartSameMonth = formatter(R.string.format_date_range_start_same_month),
           dateRangeEndSameMonth = formatter(R.string.format_date_range_end_same_month),
           dateRangeEndSameMonthWithYear =
             formatter(R.string.format_date_range_end_same_month_with_year),
+          dateRangeEndWithYear = formatter(R.string.format_date_range_end_with_year),
+        )
+      }
+
+    fun fromResourcesNarrow(resources: Resources) =
+      with(resources) {
+        Formatters(
+          yesterday = getString(R.string.yesterday_narrow),
+          today = getString(R.string.today_narrow),
+          tomorrow = getString(R.string.tomorrow_narrow),
+          lastWeek = getString(R.string.last_week_narrow),
+          thisWeek = getString(R.string.this_week_narrow),
+          nextWeek = getString(R.string.next_week_narrow),
+          day = formatter(R.string.format_day_narrow),
+          dayWithYear = formatter(R.string.format_day_with_year_narrow),
+          month = formatter(R.string.format_month_narrow),
+          monthWithYear = formatter(R.string.format_month_with_year_narrow),
+          year = formatter(R.string.format_year_narrow),
+          dateRangeStart = formatter(R.string.format_date_range_start_narrow),
+          dateRangeStartSameMonth = formatter(R.string.format_date_range_start_same_month_narrow),
+          dateRangeStartWithYear = formatter(R.string.format_date_range_start_with_year_narrow),
+          dateRangeEnd = formatter(R.string.format_date_range_end_narrow),
+          dateRangeEndSameMonth = formatter(R.string.format_date_range_end_same_month_narrow),
+          dateRangeEndSameMonthWithYear =
+            formatter(R.string.format_date_range_end_same_month_with_year_narrow),
+          dateRangeEndWithYear = formatter(R.string.format_date_range_end_with_year_narrow),
         )
       }
   }
