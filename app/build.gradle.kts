c0d4f513257fb6846e68db7a93d78fdffdfa47bf
Robin Wersich <robin.wersich@student.hpi.de>
2024-10-26 22:53:16 +0200

    Bump version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.robinwersich.todue"
     minSdk = 24
     targetSdk = 34
-    versionCode = 1
-    versionName = "0.1.1"
+    versionCode = 2
+    versionName = "0.2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables { useSupportLibrary = true }
