764d609701731309530a2133414e7039c56b5edf
Robin Wersich <robin.wersich@student.hpi.de>
2024-10-26 22:39:23 +0200

    Make background adapt to theme

--- app/src/main/kotlin/com/robinwersich/todue/ui/presentation/organizer/OrganizerScreen.kt
+++ app/src/main/kotlin/com/robinwersich/todue/ui/presentation/organizer/OrganizerScreen.kt
@@ -1,7 +1,10 @@
 package com.robinwersich.todue.ui.presentation.organizer
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.robinwersich.todue.domain.model.TimeUnit
 import com.robinwersich.todue.domain.model.Timeline
@@ -14,14 +17,17 @@ import kotlinx.collections.immutable.persistentListOf
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun OrganizerScreen(state: OrganizerState, onEvent: (OrganizerEvent) -> Unit = {}) {
-  val timelines =
-    persistentListOf(
-      Timeline(0, TimeUnit.DAY),
-      Timeline(1, TimeUnit.WEEK),
-      Timeline(2, TimeUnit.MONTH),
-    )
+  val timelines = persistentListOf(
+    Timeline(0, TimeUnit.DAY),
+    Timeline(1, TimeUnit.WEEK),
+    Timeline(2, TimeUnit.MONTH),
+  )
   val formatter = rememberTimeBlockFormatter()
-  OrganizerNavigation(timelines = timelines) { _, timeBlock ->
+  val backgroundColor = MaterialTheme.colorScheme.background
+  OrganizerNavigation(
+    timelines = timelines,
+    modifier = Modifier.background(backgroundColor)
+  ) { _, timeBlock ->
     TimeBlockView(timeBlock = timeBlock, formatter = formatter, expandProgress = { 0f })
   }
 }
