551d5029072d2bf25dcbfa3417cbfeee9c8c6a4d
Robin Wersich <33258691+robinwersich@users.noreply.github.com>
2025-09-26 17:18:27 +0200

    Upgrade gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("com.android.application") version "8.7.3" apply false
-  id("com.android.library") version "8.7.3" apply false
+  id("com.android.application") version "8.12.3" apply false
+  id("com.android.library") version "8.12.3" apply false
   kotlin("android") version "2.0.0" apply false
   kotlin("plugin.compose") version "2.0.0" apply false
   id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
