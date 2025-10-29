774078a985d012994aa15b325b201acf71c58236
Robin Wersich <33258691+robinwersich@users.noreply.github.com>
2025-10-29 23:22:21 +0100

    Bump version to 0.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     applicationId = "com.robinwersich.todue"
     minSdk = 24
     targetSdk = 35
-    versionCode = 4
-    versionName = "0.2.2"
+    versionCode = 5
+    versionName = "0.2.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables { useSupportLibrary = true }
