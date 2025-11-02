3719a86ea1beb7204a2ac6d6bfc53c2d6a6d5fb8
Robin Wersich <33258691+robinwersich@users.noreply.github.com>
2025-11-02 21:56:43 +0100

    Animate task placement

--- app/src/main/kotlin/com/robinwersich/todue/ui/presentation/organizer/components/TaskList.kt
+++ app/src/main/kotlin/com/robinwersich/todue/ui/presentation/organizer/components/TaskList.kt
@@ -1,5 +1,7 @@
 package com.robinwersich.todue.ui.presentation.organizer.components
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,6 +10,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.LookaheadScope
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.ImmutableList
@@ -30,25 +33,27 @@ fun TaskList(
     remember(modifier, onEvent) {
       modifier.clickable(interactionSource = null, indication = null) { onEvent(CollapseTasks) }
     }
-  LazyColumn(modifier = taskListModifier.padding(horizontal = 8.dp)) {
-    items(tasks, key = { it.id }) { taskState ->
-      // extract task ID so that onEvent stays the same, avoiding recomposition
-      val taskId = taskState.id
-      TaskView(
-        state = taskState,
-        onEvent = { onEvent(ModifyTask(it, taskId)) },
-        modifier =
-          when (taskState.focusLevel) {
-            FocusLevel.FOCUSSED,
-            FocusLevel.FOCUSSED_REQUEST_KEYBOARD ->
-              Modifier.clickable(interactionSource = null, indication = null) {}
-            FocusLevel.NEUTRAL ->
-              Modifier.clickable(interactionSource = null, indication = null) {
-                onEvent(ExpandTask(taskState.id))
-              }
-            FocusLevel.BACKGROUND -> Modifier
-          },
-      )
+  LookaheadScope {
+    LazyColumn(modifier = taskListModifier.padding(horizontal = 8.dp)) {
+      items(tasks, key = { it.id }) { taskState ->
+        // extract task ID so that onEvent stays the same, avoiding recomposition
+        val taskId = taskState.id
+        TaskView(
+          state = taskState,
+          onEvent = { onEvent(ModifyTask(it, taskId)) },
+          modifier =
+            when (taskState.focusLevel) {
+              FocusLevel.FOCUSSED,
+              FocusLevel.FOCUSSED_REQUEST_KEYBOARD ->
+                Modifier.clickable(interactionSource = null, indication = null) {}
+              FocusLevel.NEUTRAL ->
+                Modifier.clickable(interactionSource = null, indication = null) {
+                  onEvent(ExpandTask(taskState.id))
+                }
+              FocusLevel.BACKGROUND -> Modifier
+            }.animateItem(fadeOutSpec = spring(stiffness = Spring.StiffnessHigh)),
+        )
+      }
     }
   }
 }
